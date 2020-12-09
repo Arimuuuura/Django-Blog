00cd6c25b7734ca5a8256ed78ea47b592221c2f8
y-arimura1222 <y.arimura.carecon@gmail.com>
2020-12-10 03:16:56 +0900

    install_apps の追記

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget-tweaks', # テンプレートでホームをレンダリングするときにCSSクラスや属性を変更できるモジュール
+    'app', # 作成したアプリケーション
 ]
 
 MIDDLEWARE = [

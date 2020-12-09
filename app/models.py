247d6750a6d4ae0b53473714356de56300d0c4e6
y-arimura1222 <y.arimura.carecon@gmail.com>
2020-12-10 03:30:53 +0900

    model の作成

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model): # 投稿記事のDB
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # 投稿者, on_delete は投稿者が削除されたら記事も削除される
+    title = models.CharField("タイトル", max_length=200)
+    content = models.TextField("本文")
+    created = models.DateTimeField("作成日", default=timezone.now)
+
+    def __str__(self): # str 関数を作成することで、管理画面で表示される文字列を定義することができる
+        return self.title

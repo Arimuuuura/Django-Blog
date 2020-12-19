1fece00e82ec6c001806d99030ede6fe9655031a
y-arimura1222 <y.arimura.carecon@gmail.com>
2020-12-20 01:58:01 +0900

    context_processors.py の修正

--- app/context_processors.py
+++ app/context_processors.py
@@ -2,7 +2,7 @@ from .models import Category
 
 # 全てのカテゴリデータをどのテンプレートでも取得できるようにする
 def common(request):
-    category_data = Category.object.all()
+    category_data = Category.objects.all()
     context = {
         'category_data': category_data
     }
